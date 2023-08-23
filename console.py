6d88d7672cbead21c13510990834da6b0ff98883
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-08-23 22:57:14 +0000

    Fix pycodestyle error and make executable

--- console.py
+++ console.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 """ Console Module """
 import cmd
 import sys
@@ -187,7 +189,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -319,6 +321,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
