5beddc063243da9c05eda45d3c1ffdd3c189acc1
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-08-27 20:35:04 +0000

    Added sql files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,11 @@
--This script prepares a MySQL server
+-- This script prepares a MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
---This creates a new user
+-- This creates a new user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
---Grant privileges
-GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
-GRANT SELECT TO performance_schema . * ON 'hbnb_test'@'localhost';
+-- Grant privileges
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
