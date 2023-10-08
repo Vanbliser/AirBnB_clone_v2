d2df8e13652a48b8fc9f203848e3dcee7318dca3
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-10-08 23:00:56 +0000

    task 101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -6,82 +6,67 @@ $ERROR_COMMENT='\t# Custom error page'
 $CUSTOM_ERROR='\terror_page 404 /custom_404.html;\n\tlocation = /custom404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}'
 
 package { 'nginx':
-  ensure => installed,
+  ensure   => installed,
 }
 
-file_line { 'redirect_me':
+-> file_line { 'redirect_me':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me/?$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-file_line { 'custom_header':
+-> file_line { 'custom_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'add_header X-Served-By 6684-web-01;',
 }
 
-exec { 'static_config'
+-> exec { 'static_config'
   path    => '/usr/bin:/bin',
-  command => 'sudo sed -i "/server_name _/a\\\n$STATIC_COMMENT\n$STATIC_CONFIG" /etc/nginx/sites-available/default'
+  command => 'sudo sed -i "/server_name _/a\\\n$STATIC_COMMENT\n$STATIC_CONFIG" /etc/nginx/sites-available/default',
 }
 
-exec { 'custom_error'
+-> exec { 'custom_error'
   path    => '/usr/bin:/bin',
-  command => 'sudo sed -i "/server_name _;/a\\\n\n$ERROR_COMMENT\n$CUSTOM_ERROR" /etc/nginx/sites-available/default'
+  command => 'sudo sed -i "/server_name _;/a\\\n\n$ERROR_COMMENT\n$CUSTOM_ERROR" /etc/nginx/sites-available/default',
 }
 
-file { '/data':
-  ensure  => 'directory',
-  owner   => 'ubuntu'
-}
-
-file { '/data/web_static':
-  ensure => 'directory'
-  owner   => 'ubuntu'
-}
-
-file { '/data/web_static/releases':
-  ensure => 'directory'
-  owner   => 'ubuntu'
-}
-
-file { '/data/web_static/releases/test':
-  ensure => 'directory'
-  owner   => 'ubuntu'
-}
-
-file { '/data/web_static/shared':
-  ensure => 'directory'
-  owner   => 'ubuntu'
-}
-
-file { '/data/web_static/releases/test/index.html':
+-> file { 'home_page':
   ensure  => 'present',
-  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
-  owner   => 'ubuntu'
-}
-
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test'
-  owner   => 'ubuntu'
-}
-
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
-}
-
-file { 'home_page':
   path    => '/var/www/html/index.html',
   content => 'Hello World!',
 }
 
-service { 'nginx':
+-> exec { 'create_folders':
+  command => '/usr/bin/bash mkdir -p /data/web_static/releases/test/',
+}
+
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n",
+  owner   => 'ubuntu',
+  mode    => '0644',
+}
+
+-> file { '/data/web_static/shared':
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  mode    => '0775',
+}
+
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
+  owner   => 'ubuntu',
+}
+
+-> exec { 'set_permissions':
+  command => '/usr/bin/bash chown -R ubuntu:ubuntu /data'
+}
+
+-> service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
 }
-
-File['/data'] -> File['/data/web_static'] -> File['/data/web_static/releases'] -> File['/data/web_static/releases/test'] -> File['/data/web_static/releases/test/index.html'] -> File['/data/web_static/shared'] -> File['/data/web_static/current'] -> Package['nginx'] -> File_line['redirect_me'] -> File_line['custom_header'] -> Exec['custom_error'] -> Exec['custom_error'] -> File['home_page'] -> Service['nginx']
